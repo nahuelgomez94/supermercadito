4ebbf2684e3d97812b020f2a512734a43e530b68
nahuelgomez94 <nahuel.gomez@mercadolibre.com>
2023-01-11 13:04:34 -0300

    Lectura del dotenv

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"log"
+
 	"github.com/bootcamp/supermercadito/internal/producto"
 	"github.com/bootcamp/supermercadito/internal/routers"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	server := gin.Default()
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error al cargar el archivo .env")
+	}
 
 	server.GET("/ping", func(c *gin.Context) {
 		c.String(200, "pong")
